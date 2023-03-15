b1edc70e4cbd9e01fa1626585e03b2a2549df1c2
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-15 18:36:23 -0400

    completes github login option

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -106,6 +106,7 @@ const Auth = () => {
                                     <FcGoogle size={30}/>
                                 </div>
                                 <div
+                                    onClick={() => signIn('github', { callbackUrl: '/' })}
                                     className="
                                         w-10
                                         h-10
