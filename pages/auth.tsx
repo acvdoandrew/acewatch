9153a02d780f2be13dfa6f31d877470457e62645
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-15 18:38:25 -0400

    completes google auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -90,6 +90,7 @@ const Auth = () => {
                         </button>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                                 <div
+                                    onClick={() => signIn('google', { callbackUrl: '/' })}
                                     className="
                                         w-10
                                         h-10
