9467ea2487283021ef1cfb123992e98026ad9882
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-16 15:21:13 -0400

    adjusts redirects and callbacksurl to profiles

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,13 +2,10 @@ import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import axios from "axios";
 import { signIn } from 'next-auth/react';
-import { useRouter } from "next/router";
-
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
-    const router = useRouter();
 
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
@@ -25,15 +22,12 @@ const Auth = () => {
             await signIn('credentials', {
                 email,
                 password,
-                redirect: false,
-                callbackUrl: '/'
+                callbackUrl: '/profiles'
             });
-
-            router.push('/');
         } catch (error) {
             console.log(error)
         }
-    },[email, password, router])
+    },[email, password])
 
     const register = useCallback(async () => {
         try {
@@ -90,7 +84,7 @@ const Auth = () => {
                         </button>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                                 <div
-                                    onClick={() => signIn('google', { callbackUrl: '/' })}
+                                    onClick={() => signIn('google', { callbackUrl: '/profiles' })}
                                     className="
                                         w-10
                                         h-10
@@ -107,7 +101,7 @@ const Auth = () => {
                                     <FcGoogle size={30}/>
                                 </div>
                                 <div
-                                    onClick={() => signIn('github', { callbackUrl: '/' })}
+                                    onClick={() => signIn('github', { callbackUrl: '/profiles' })}
                                     className="
                                         w-10
                                         h-10
