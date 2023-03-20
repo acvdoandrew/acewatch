8239d9e1d8d1021bbbdff8cfe708016aa2cfd395
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-20 16:18:44 -0400

    adds favorite button to moviecard UI

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
+import FavoriteButton from "./FavoriteButton";
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -84,6 +85,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                         onClick={() => {}} >
                             <BsFillPlayFill size={30} />
                         </div>
+                        <FavoriteButton movieId={data?.id} />
                     </div>
                     <p className="text-green-400 font-semibold mt-4">
                         New <span className="text-white">2023</span>
