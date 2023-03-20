72d0e89eb2d6c320404522fedd890fc04afa3784
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-20 17:16:37 -0400

    adds play button functionality to billboard

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -2,6 +2,7 @@ import useBillboard from "@/hooks/useBillboard";
 import React from "react";
 
 import { AiOutlineInfoCircle } from 'react-icons/ai';
+import PlayButton from "./PlayButton";
 
 const Billboard = () => {
     const { data } = useBillboard();
@@ -47,6 +48,7 @@ const Billboard = () => {
                         {data?.description}
                     </p>
                     <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+                        <PlayButton movieId={data?.id} />
                        <button className="
                         bg-white
                         text-white
