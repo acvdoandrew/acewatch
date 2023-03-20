ce587073bed9a296466b3c0833bc4b89b72f8ffb
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-20 17:16:13 -0400

    routes to a video link to play

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
+import { useRouter } from "next/router";
 
 interface MovieCardProps {
     data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+    const router = useRouter();
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
             <img 
@@ -82,7 +84,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                             transition
                             hover:bg-neutral-300
                         "
-                        onClick={() => {}} >
+                        onClick={() => router.push(`/watch/${data?.id}`)} >
                             <BsFillPlayFill size={30} />
                         </div>
                         <FavoriteButton movieId={data?.id} />
