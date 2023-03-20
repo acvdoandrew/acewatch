aef9bf44783d7ebd263699d277a4ab3090cfc7f1
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-20 16:18:28 -0400

    adds my list to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
 
  export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,12 +25,14 @@ import useMovieList from "@/hooks/useMovieList";
 
  export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   )
