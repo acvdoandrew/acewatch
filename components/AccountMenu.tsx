bd590e2b3775476afa0ff50982607bfd93d479c5
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-03-20 18:06:01 -0400

    renders account name

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { signOut } from "next-auth/react";
 import React from "react";
 
@@ -6,6 +7,8 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+    const { data } = useCurrentUser();
+
     if (!visible) {
         return null;
     }
@@ -15,7 +18,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/default-red.png" alt="" />
                     <p className="text-white text-sm group-hover/item:underline">
-                        Username
+                        {data?.name}
                     </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4" />
